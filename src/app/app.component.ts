aa8c3a4e6e81a30d59bb92bb3a83180255b01b4f
Adrian Juszczak <adrian.juszczak@cdv.pl>
2021-04-24 10:37:00 +0200

    Templates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'cdv-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'Angular Intro';
+  public title = 'Angular Intro';
+  public name = 'Adrian';
+  public isDisabled = false;
+  public count = 0;
+
+  constructor() {}
+
+  onButtonClicked($event: MouseEvent): void {
+    console.log('Click', $event);
+    this.count++;
+  }
+
+  onEnter($event: MouseEvent): void {
+    console.log($event.type);
+  }
+
+  onInputChange(value: string): void {
+    this.name = value;
+  }
 }
