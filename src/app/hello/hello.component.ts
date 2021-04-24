1bb56ebbd9e1ad5d8d50f5c42b8f2472b148b16a
Adrian Juszczak <adrian.juszczak@cdv.pl>
2021-04-24 13:15:34 +0200

    Lifecycle Hooks

--- src/app/hello/hello.component.ts
+++ src/app/hello/hello.component.ts
@@ -1,4 +1,18 @@
-import { Component, EventEmitter, Input, OnInit, Output, ViewEncapsulation } from '@angular/core';
+import {
+  AfterContentInit,
+  AfterViewChecked,
+  AfterViewInit,
+  Component,
+  EventEmitter,
+  Input,
+  OnChanges,
+  OnDestroy,
+  OnInit,
+  Output,
+  SimpleChange,
+  SimpleChanges,
+  ViewEncapsulation,
+} from '@angular/core';
 
 @Component({
   selector: 'app-hello',
@@ -6,11 +20,12 @@ import { Component, EventEmitter, Input, OnInit, Output, ViewEncapsulation } fro
   styleUrls: ['./hello.component.scss'],
   encapsulation: ViewEncapsulation.Emulated,
 })
-export class HelloComponent implements OnInit {
+export class HelloComponent implements OnChanges, AfterViewChecked, AfterViewInit, AfterContentInit, OnInit, OnDestroy {
   public static counter = 0;
   public id: number;
   @Input() public heading: string;
   @Input() public expand = false;
+  @Input() public clicks: number;
 
   @Output() public headingClick = new EventEmitter<number>();
 
@@ -19,7 +34,31 @@ export class HelloComponent implements OnInit {
     this.id = HelloComponent.counter;
   }
 
-  ngOnInit(): void {}
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('OnChanges', this.id, changes);
+    const clicks: SimpleChange = changes.clicks;
+    console.log(clicks.previousValue, '->', clicks.currentValue);
+  }
+
+  ngOnInit(): void {
+    console.log('OnInit', this.id);
+  }
+
+  ngAfterContentInit(): void {
+    console.log('AfterContentInit', this.id);
+  }
+
+  ngAfterViewInit(): void {
+    console.log('AfterViewInit', this.id);
+  }
+
+  ngAfterViewChecked(): void {
+    console.log('AfterViewChecked', this.id);
+  }
+
+  ngOnDestroy(): void {
+    console.log('OnDestroy', this.id);
+  }
 
   public onHeadingClick($event: MouseEvent): void {
     this.headingClick.emit(this.id);
