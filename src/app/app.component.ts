30a3921bdf4e63a63ed24f740c5043f44ae880e0
Adrian Juszczak <adrian.juszczak@cdv.pl>
2021-04-24 15:14:58 +0200

    Directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,6 +12,20 @@ export class AppComponent {
   public showTemplates = false;
   public clicks = 0;
 
+  get previewClassNames(): any {
+    return {
+      even: this.clicks % 2 === 0,
+      odd: this.clicks % 2 !== 0,
+      three: this.clicks % 3 === 0,
+    };
+  }
+
+  get previewStyles(): any {
+    return {
+      'border-radius': `${this.clicks * 10}%`,
+    };
+  }
+
   constructor(private router: Router, @Inject(APP_CONFIG) private config: AppConfig) {}
 
   public onHelloHeadingClick(clicks: number): void {
