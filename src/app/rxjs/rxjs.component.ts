8944710d4b0f48c58a32b28eb7f84f00e8b3c342
Adrian Juszczak <adrian.juszczak@cdv.pl>
2021-04-25 10:38:44 +0200

    Operators

--- src/app/rxjs/rxjs.component.ts
+++ src/app/rxjs/rxjs.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { from, fromEvent, interval, Observable, of } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { filter, map, mergeMap, switchMap, take, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-rxjs',
@@ -25,7 +25,23 @@ export class RxjsComponent implements OnInit {
       map((event: MouseEvent) => [event.x, event.y]),
       map((coords: [number, number]) => coords[1]),
     );
-
     clicks$.subscribe(value => console.log(value));
+
+    const numbers$ = of(0, 1, 2, 3, 4, 5).pipe(
+      filter(value => value % 2 !== 0),
+      map(value => value * 2),
+    );
+
+    // numbers$.subscribe(console.log);
+
+    this.intarval$.pipe(
+      filter(value => value % 3 === 0),
+      take(5),
+    );
+
+    const int$ = this.clicks$.pipe(
+      map(value => interval(1000)),
+      switchMap(value => value),
+    );
   }
 }
